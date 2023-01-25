a5a9a4e378351b6636b4ba4412b3c35cd84e6e9d
Eugene Odeluga <eugene.odeluga@agent3.com>
2023-01-25 19:32:10 +0000

    Fixed React hydration errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,13 +11,11 @@ function Header() {
     <header>
       {/* Top nav */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
-        <div className="mt-2 flex items-center flex-grow sm:flex-grow">
-          <Image
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow object-fill">
+          <Image className="link object-fill"
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
-            objectFit="contain"
-            className="link"
           />
         </div>
         {/* Search */}
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StarIcon } from '@heroicons/react/solid';
 import Currency from 'react-currency-formatter';
 
@@ -8,38 +7,42 @@ const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({ id, title, price, description, category, image }) {
-    const [rating] = useState(
-        Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 11)) + MIN_RATING
-    );
-
-    const [hasPrime] = useState(Math.random() < 0.5)
-
+    
+    const [rating, setRating] = useState(0);
+    const [hasPrime, setHasPrime] = useState(0)
+    const randomVal = Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 11)) + MIN_RATING;
+    
+    useEffect(() => {
+        setRating(randomVal)
+        setHasPrime(Math.random() < 0.5);
+    });
+    
     return (
+        
         <div className="relative flex-col m-5 bg-white z-30 p-10">
             <p className="absolute top-2 right-2 text-xs italic text-gray-400">
                 {category}
             </p>
-            <Image
+            <Image className='object-fill'
                 src={image}
                 height={200}
-                width={200}
-                objectFit="contain" />
-
+                width={200} />
             <h4 className="my-3">{title}</h4>
             
             <div className="flex">
                 {Array(rating)
                     .fill()
                     .map((_, i) => (
-                        <StarIcon className="h-5 text-yellow-500" />
+                        <StarIcon key={i} className="h-5 text-yellow-500"/>
                     ))}
             </div>
 
             <p className="text-xs my-2 line-clamp-2">{description}</p>
-
-            <div className="mb-5">
-                <Currency quantity={price} currency="GBP" />
-            </div>
+            {
+                <div className="mb-5">
+                    <Currency quantity={price} currency="GBP" />
+                </div>
+            }
 
             {hasPrime && (
                 <div className="flex items-center space-x-2 -mt-5">
